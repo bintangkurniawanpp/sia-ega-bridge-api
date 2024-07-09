7450ba576997095fb4d9f8e06f9db8871b653d98
bintang <nnew34004@gmail.com>
2024-07-09 11:18:01 +0700

    refactor service to utils

--- src/services/instansi-service.js
+++ src/services/instansi-service.js
@@ -1,76 +1,8 @@
-import https from "https";
-import axios from "axios";
 import logger from "../application/logger.js";
 import { getMainGroupGUID } from "./arisdb-service.js";
-import config from "../config/config.js";
 import { ResponseError } from "../error/response-error.js";
+import { getGroupChildren, getNestedGroups } from "../utils/common-util.js";
 
-// Axios Setup
-const axiosInstance = axios.create({
-  httpsAgent: new https.Agent({ rejectUnauthorized: false }),
-  headers: { "Content-Type": "application/x-www-form-urlencoded" },
-});
-
-// Get Group Children
-async function getGroupChildren(dbName, groupID, accessToken) {
-  const { absUrl } = config[process.env.NODE_ENV] || config.development;
-  const url = `${absUrl}/groups/${dbName}/${groupID}/children`;
-  const params = {
-    umcsession: accessToken,
-    orderby: "name",
-    attributes: "all",
-    withobjects: "false",
-    withparentgroup: "true",
-  };
-
-  const headers = {
-    Authorization: `Bearer ${accessToken}`, // Pass the token in the Authorization header
-  };
-
-  try {
-    logger.info(`Getting children of group ${groupID} from database ${dbName}`);
-    const response = await axiosInstance.get(url, { params, headers });
-    return response.data;
-  } catch (error) {
-    if (error.response) {
-      logger.error(`Error getting children: ${error.response.status} ${error.response.statusText}`);
-      throw new ResponseError(error.response.status, error.response.statusText);
-    } else {
-      logger.error(`Error getting children: ${error.message}`);
-      throw error;
-    }
-  }
-}
-
-// Get nested groups up to a certain depth recursively
-const getNestedGroups = async (dbName, groupID, accessToken, currentDepth, maxDepth) => {
-  if (currentDepth > maxDepth) {
-    return [];
-  }
-
-  const children = await getGroupChildren(dbName, groupID, accessToken);
-  const nestedGroups = await Promise.all(
-    children.items.map(async (item) => {
-      if (item.kind === "GROUP") {
-        const nested = await getNestedGroups(dbName, item.guid, accessToken, currentDepth + 1, maxDepth);
-        const idAttribute = item.attributes.find(attr => attr.apiname === "AT_ID");
-        const group = {
-          guid: item.guid,
-          name: item.attributes.find((attr) => attr.apiname === "AT_NAME").value,
-          id_kl: idAttribute ? idAttribute.value : null,
-        };
-        if (nested.length > 0) {
-          group.instansi = nested;
-        }
-        return group;
-      } else {
-        return null;
-      }
-    })
-  );
-
-  return nestedGroups.filter((group) => group !== null);
-};
 
 // Get all instansi
 export const getAllInstansi = async (accessToken, dbName) => {
